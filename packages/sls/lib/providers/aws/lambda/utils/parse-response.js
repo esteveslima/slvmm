b3aca44c2d2483d6c6912b601ea1df5d8d848620
Esteves <esteveslima@ESTEVES-UBUNTU>
2021-05-08 20:11:22 -0300

    fix(@sls/lib): fix lambda parse response
    fix lambda parse response, to accept encoded files(base64)

--- packages/sls/lib/providers/aws/lambda/utils/parse-response.js
+++ packages/sls/lib/providers/aws/lambda/utils/parse-response.js
@@ -5,10 +5,14 @@ export default (functionResult) => {
       return {
         statusCode: functionResult.statusCode || 200,
         headers: functionResult.headers || undefined,
+        isBase64Encoded: functionResult.isBase64Encoded || undefined,
         body: ((obj) => {
+          if (obj.isBase64Encoded && typeof obj.body === 'string') return obj.body;
+
           const resultObj = JSON.parse(JSON.stringify(obj));
           delete resultObj.statusCode;
           delete resultObj.headers;
+          delete resultObj.isBase64Encoded;
           return Object.keys(resultObj).length > 0 ? JSON.stringify(resultObj) : undefined;
         })(functionResult),
       };
