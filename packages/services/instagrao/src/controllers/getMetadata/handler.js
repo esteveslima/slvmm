33b753ec81a6b928e21b21aaa986092a77e80ddd
Esteves <esteveslima@ESTEVES-UBUNTU>
2021-05-09 21:55:07 -0300

    feat(instagrao): get image metadata function

--- packages/services/instagrao/src/controllers/getMetadata/handler.js
+++ packages/services/instagrao/src/controllers/getMetadata/handler.js
@@ -1,10 +1,12 @@
 import { lambda, logger, middleware } from '@sls/lib';
+import * as imagesDao from '../../common/database/dao/images-dao';
 
 middleware.before((event) => { logger.log('middleware usage example'); });
 
 export default lambda(async (event) => {
-  const result = 123;
-  return {
-    someResult: result,
-  };
+  const { s3objectkey } = event.pathParameters;
+
+  const imageMetadata = await imagesDao.getImageInfo(s3objectkey);
+
+  return imageMetadata;
 });
